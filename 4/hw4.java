70841e549d5ae270d51c8d2c3993fa909742e278
Sergey <barmaley78@mail.ru>
2023-03-09 19:31:24 +0700

    finished homeworks 4, 5, 6

--- 4/hw4.java
+++ 4/hw4.java
@@ -12,27 +12,13 @@ public class hw4 {
         XMLFormatter xml = new XMLFormatter();
         fh.setFormatter(xml);
         logger.info("Sort Array");
-        int[] array = new int[100];
+        int[] array = new int[20];
         fillArray(array, 0, 100);
         printArrayInt(array);
-        // int[] newArr = listSort(array);
         System.out.printf("\n Отсортированный массив: \n");
-        // printArrayInt(newArr);
+        heapSort(array);
+        printArrayInt(array);
     }
-
-    // public static int[] listSort(int [] arr){
-    //     for(int k = 0; k < arr.length-1; k++) {
-            
-    //         for (int i = 0; i < arr.length - k-1; i++){
-    //             if (arr[i] > arr[i+1]) {
-    //                 int temp = arr[i];
-    //                 arr[i] = arr[i+1];
-    //                 arr[i+1] = temp;
-    //             }
-    //         }
-    //     }
-    //     return arr;
-    // }
 
     public static void printArrayInt(int[] arr) {
         for (int i = 0; i < arr.length-1; i++) {
@@ -47,5 +33,75 @@ public class hw4 {
           col[index] = rnd.nextInt(min, max);
           index = index + 1;
         }
-      }
+    }
+
+    //вернуть левого потомка `A[i]`
+    private static int Left(int i) {
+        return (2 * i + 1);
+    }
+
+//вернуть правого потомка `A[i]`
+    private static int Right(int i) {
+        return (2 * i + 2);
+    }
+
+//вспомогательная функция для замены двух индексов в массиве
+    private static void swap(int[] sortArr, int i, int j) {
+        int swap = sortArr[i];
+        sortArr[i] = sortArr[j];
+        sortArr[j] = swap;
+    }
+
+//рекурсивный алгоритм heapify-down. Узел с индексом `i` и 2 его прямых потомка нарушают свойство кучи
+    private static void heapify(int[] sortArr, int i, int size) {
+        // получить левый и правый потомки узла с индексом `i`
+        int left = Left(i);
+        int right = Right(i);
+        int largest = i;
+
+        //сравниваем `A[i]` с его левым и правым дочерними элементами и находим наибольшее значение
+        if (left < size && sortArr[left] > sortArr[i]) largest = left;
+        if (right < size && sortArr[right] > sortArr[largest]) largest = right;
+
+        //поменяться местами с потомком, имеющим большее значение и вызовите heapify-down для дочернего элемента
+        if (largest != i) {
+            swap(sortArr, i, largest);
+            heapify(sortArr, largest, size);
+        }
+    }
+
+//функция для удаления элемента с наивысшим приоритетом (присутствует в корне)
+    public static int pop(int[] sortArr, int size) {
+        //если в куче нет элементов
+        if (size <= 0) {
+            return -1;
+        }
+        int top = sortArr[0];
+
+        //заменяем корень кучи последним элементом массива
+        sortArr[0] = sortArr[size-1];
+        //вызовите heapify-down на корневом узле
+        heapify(sortArr, 0, size - 1);
+        return top;
+    }
+
+//функция для выполнения пирамидальной сортировки массива `A` размера `n`
+    public static void heapSort(int[] sortArr) {
+        //строим приоритетную очередь и инициализируем ее заданным массивом
+        int n = sortArr.length;
+
+        //build-heap: вызывать heapify, начиная с последнего внутреннего
+        //узел до корневого узла
+        int i = (n - 2) / 2;
+        while (i >= 0) {
+            heapify(sortArr, i--, n);
+        }
+
+        //несколько раз извлекаем из кучи, пока она не станет пустой
+        while (n > 0) {
+            sortArr[n - 1] = pop(sortArr, n);
+            n--;
+        }
+    }
+
 }
